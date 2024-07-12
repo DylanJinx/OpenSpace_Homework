006534807e154d5c7821930497ff2dcfc65fd09a
DylanJinx <2636353989@qq.com>
2024-07-13 01:45:28 +0800

    visitNFT-update

---
+++ W2D5/visitNFT/README.md
@@ -0,0 +1,5 @@
+```shell
+➜  visitNFT git:(main) ✗ node oribit_frontend/dist/oribit_frontend/services/nftService.js
+Orbit NFT is owned by: 0x6897625C2Da7E985e9c22E0d7B27A960Fc81D1D2
+tokenId = 1 URI is: ipfs://QmY9wa5FssaBBhLyyC2r649rwfS7CcvH7NG5AJWepeDkGj/1.json
+```
---
+++ W2D5/visitNFT/oribit_frontend/services/nftService.ts
@@ -0,0 +1,37 @@
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains';
+import  { abi } from '../../OrbitNFTBackend/out/OrbitNFT.sol/Orbit.json'; 
+import dotenv from 'dotenv';
+
+async function main() {
+    const client = createPublicClient({ 
+        chain: mainnet, 
+        transport: http(), 
+    })
+
+    const nftContractAddress = '0x0483b0dfc6c78062b9e999a82ffb795925381415'
+    const tokenId = 1
+
+    const owner = await client.readContract({
+        address: nftContractAddress,
+        abi: abi,
+        functionName: 'ownerOf',
+        args: [tokenId]
+    })
+
+    console.log("Orbit NFT is owned by:", owner)
+
+    const tokenURI = await client.readContract({
+        address: nftContractAddress,
+        abi: abi,
+        functionName: 'tokenURI',
+        args: [tokenId]
+    })
+
+    console.log("tokenId = 1 URI is:", tokenURI)
+}
+
+main().catch(error => {
+    console.error("Uncaught Error:", error);
+});
+
