6a8af4fe15a4f35df02a1a1b70379a9b96deda40
DylanJinx <2636353989@qq.com>
2024-07-12 17:08:44 +0800

    w2d5-update

--- W2D5/deployERC20/script/MyTokenScipt.sol
+++ W2D5/deployERC20/script/MyTokenScipt.sol
@@ -2,18 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {MyToken} from "../src/MyToken.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    MyToken public myTokenContract;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        myTokenContract = new MyToken("Dylan Token", "DJ");
 
         vm.stopBroadcast();
+
+        console.log("Contracts have benn deployed to: ", address(myTokenContract));
     }
 }
---
+++ W2D5/deployERC20/script/deploy.sh
@@ -0,0 +1,12 @@
+# 需要在foundry.toml中设置[rpc_endpoints]设置local = "http://hostlocal:8545"
+forge create --rpc-url local --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 src/MyToken.sol:MyToken --constructor-args "Dylan Token" "DJ"
+
+# 连接本地节点
+curl -X POST --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[],"id":67}' http://127.0.0.1:8545
+
+
+# 访问sepolia查询余额 0xb9cee3a6842ad712 = 13.38888904654054 ETH
+curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBalance","params":["0x3A8492819b0C9AB5695D447cbA2532b879d25900", "latest"],"id":1}' https://1rpc.io/sepolia
+
+# 访问ETH查询余额 0x2f61bffb382cc1 = 0.013336801086811328 ETH
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getBalance","params":["0x3A8492819b0C9AB5695D447cbA2532b879d25900", "latest"],"id":1}' https://eth.drpc.org
