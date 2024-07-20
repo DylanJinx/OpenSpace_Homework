18124956c1d3bb6c02b6559b6c281ce8a3ab6f88
DylanJinx <2636353989@qq.com>
2024-07-20 18:50:36 +0800

    update w3d5

---
+++ W3D5/DYLAN_NFTMARKET/src/MyToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20Permit {
+    uint256 public maxSupply = 1000000 * 10 ** 18;
+
+    constructor() ERC20("DylanToken", "DT") ERC20Permit("DylanToken") {
+        _mint(msg.sender, maxSupply);
+    }
+
+    // 公开Digest
+    function getDigest(bytes32 structHash) public view returns (bytes32) {
+        return _hashTypedDataV4(structHash);
+    }
+}
