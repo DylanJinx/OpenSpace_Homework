153675ef27f9dd63d0da10017bd065eb8aeb37c3
DylanJinx <2636353989@qq.com>
2024-07-24 02:07:45 +0800

    remote IRNTToken

--- W4D2/IDO_STAKE/src/IRNTToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-import {IERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import {IERC20Permit} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Permit.sol";
-interface IRNTToken is IERC20, IERC20Permit {
-    function mintForIDO(uint256 _amount) external;
-
-}
